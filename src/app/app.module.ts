53e0ced46b39fe4741ad3849fdc118073aa44416
Vega Berna <glorvega93@hotmail.com>
2023-10-04 17:14:44 +0200

    advances structure with modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InterfacesComponent } from './interfaces/interfaces.component';
+import { FooterModule } from './core/footer/footer.module';
+import { NavbarModule } from './core/navbar/navbar.module';
+import { DetailModule } from './pages/detail/detail.module';
+import { HomeModule } from './pages/home/home.module';
+import { ListModule } from './pages/list/list.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InterfacesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FooterModule,
+    NavbarModule,
+    DetailModule,
+    ListModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
